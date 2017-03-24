fdea6692049958af5003ee4affe548415c480369
asuishi <1962323664@qq.com>
2017-03-24 15:04:58 +0800

    data proxy

--- src/main.js
+++ src/main.js
@@ -3,7 +3,27 @@ import compiler from "./compiler"
 
 export default class myvue{
 	constructor(element,data){
-		this._data = (new observer(data)).data;
+		// this._data = (new observer(data)).data;
+		let _data = new observer(data);
+		this._proxy(_data,this)
 		new compiler(element,data);
+	}
+
+	_proxy(data,vm){
+		for(let d in data.data){
+			Object.defineProperty(vm,d,{
+				enumerable  : true,    // 枚举
+	        	configurable: false,   // 不可再配置
+				get(){
+					return data.data[d];
+				},
+				set(v){
+					if(v == data.data[d]){
+						return;
+					}
+					data.data[d] = v;	
+				}
+			});
+		}
 	}
 }
